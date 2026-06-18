93087253665373735fe2d526f66c3915adece66a
Sheevan Ul Haq <sheevan12345@gmail.com>
2026-06-18 21:01:39 +0530

    Project Setup

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { specialityData } from "../assets/assets.js";
+
+const SpecialityMenu = () => {
+  return (
+    <div
+      id="speciality"
+      className="flex flex-col items-center gap-4 py-16 text-gray-800"
+    >
+      <h1 className="text-3xl font-medium">Find By Speciality</h1>
+      <p className="text-center md:w-1/3 text-sm">
+        Simply browse through our extensive list of trusted doctors, schedule
+        your appointment hassle-free.
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, idx) => (
+          <Link
+            className="flex flex-col items-center text-xs cursor-pointer shrink-0 hover:-translate-y-2 transition-all duration-500"
+            to={`/doctors/${item.speciality}`}
+            key={idx}
+            onClick={() => scrollTo(0, 0)}
+          >
+            <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
+            <p>{item.speciality}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SpecialityMenu;
---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { doctors } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const TopDoctors = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+      <p className="sm:w-1/3 text-sm text-center">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
+      <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+        {doctors.slice(0, 10).map((doctor, idx) => (
+          <div
+            key={idx}
+            onClick={() => navigate(`/appointment/${doctor._id}`)}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2.5 transition-all duration-500"
+          >
+            <img
+              className="bg-violet-200/30 rounded-t-2xl"
+              src={doctor.image}
+              alt=""
+            />
+            <div className="p-4">
+              <div className="text-green-500 flex gap-2 items-center text-sm text-center">
+                <div className="w-2 h-2 rounded-full bg-green-600" />
+                <p>Available</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
+              <p className="text-gray-600 text-sm">{doctor.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+        more
+      </button>
+    </div>
+  );
+};
+
+export default TopDoctors;
